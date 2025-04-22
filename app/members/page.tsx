1ad0ed667ba4ff1eceb2a0738f5f26b87bf010d6
marudan55 <kazuma.otokozawa@gmail.com>
2025-04-22 22:55:12 +0900

    5章まで完了

---
+++ app/members/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMemberList } from "@/app/_libs/microcms";
+export default async function Page() {
+  const data = await getMemberList();
+
+  return (
+    <div className={styles.container}>
+      {data.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dd className={styles.position}>{member.position}</dd>
+                <dd className={styles.profile}>{member.profile}</dd>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
