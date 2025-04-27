462a639fc146c9fa9d13be687862e4a8a1be50d8
marudan55 <kazuma.otokozawa@gmail.com>
2025-04-27 17:07:25 +0900

    GA4設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-C76XWZTBE5" />
     </html>
   );
 }
